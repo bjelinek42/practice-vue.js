fced5db95039735fc0c622168a810c4de35306f9
ben <bjelinek42@gmail.com>
2022-03-31 18:41:24 -0600

    class exercises

---
+++ index.js
@@ -0,0 +1,22 @@
+/* global Vue */
+
+var App = {
+  data: function() {
+    return {
+      message: "Hello from JavaScript!",
+      name: "Peter",
+      city: "colorado springs"
+    };
+  },
+  methods: {
+    doThis: function() {
+      console.log("this is text");
+    },
+    changeName: function() {
+      console.log('changing the name');
+      this.name = "Jessica";
+    }
+  }
+};
+
+Vue.createApp(App).mount('#app');
